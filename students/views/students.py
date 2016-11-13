94a72927a072e028eb9304a190978784095ec476
trivvet <trivvet@gmail.com>
2016-11-13 22:14:44 +0200

    Chapter 7 Add Django Pagination

--- students/views/students.py
+++ students/views/students.py
@@ -2,6 +2,8 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from ..models import Student
 
 # Student List
@@ -12,11 +14,26 @@ def students_list(request):
     
     # try to order student list
     order_by = request.GET.get('order_by', '')
-    if order_by in ('last_name', 'first_name', 'ticket'):
+    if order_by in ('last_name', 'first_name', 'ticket', 'id'):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
+    else:
+        students = students.order_by('last_name')
 
+    # paginate students
+    paginator = Paginator(students, 3)
+    page = request.GET.get('page')
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page
+        students = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver
+        # last page of result
+        students = paginator.page(paginator.num_pages)
+    
     groups = (
          {'id': 1,
           'name': u'БМ - 1',
