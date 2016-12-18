016722de38811006ee3b8522be22fce2a0f54938
trivvet <trivvet@gmail.com>
2016-12-18 21:08:09 +0200

    Chapter 9 Add Ajax Navigation

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -50,10 +50,112 @@ function initGroupSelector() {
 }
 
 function initDateFields() {
+  var defaultDate = $('input.dateinput').val();
+  if (!defaultDate) {
+    defaultDate = '1998-01-01'
+  }
   $('input.dateinput').datetimepicker({
-    'format': 'YYYY-MM-DD'
+    'useCurrent': false,
+    'format': 'YYYY-MM-DD',
+    'viewDate': defaultDate
   }).on('dp.hide', function(event) {
     $(this).blur();
+  });
+}
+
+function initEditStudentForm(form, modal, link) {
+  // attach datepicker
+  initDateFields();
+
+  // close modal window on Cancel button click
+  form.find('input[name="cancel_button"]').click(function(event) {
+    modal.modal('hide');
+    return false;
+  });
+
+  // make form work in AJAX mode
+  form.ajaxForm({
+    url: link.attr('href'),
+    dataType: 'html',
+    error: function() {
+      alert('Помилка на сервері. Спробуйте будь-ласка пізніше');
+      return false;
+    },
+    success: function(data, status, xhr) {
+      var html = $(data), newform = html.find('#content-column form.form-horizontal');
+
+      // copy alert to modal window
+      modal.find('.modal-body').html(html.find('.alert'));
+
+      // copy form to modal if we found it in server repsonse
+      if (newform.length > 0) {
+        modal.find('.modal-body').append(newform);
+
+        // initialize form fields and buttons
+        initEditStudentForm(newform, modal, link)
+      } else {
+        // if no form, it means success and we need to reload page
+        // to get updated students list;
+        // reload after 2 second, so that user can read
+        // success message
+        setTimeout(function(){location.reload(true);}, 500);
+      }
+    }
+  });
+}
+
+function initEditStudentPage() {
+  $('a.student-edit-form-link').click(function(){
+    var link = $(this)
+    $.ajax({
+      'url': link.attr('href'),
+      'dataType': 'html',
+      'type': 'get',
+      'success': function(data, status, xhr) {
+        if (status != 'success') {
+          alert('Помилка на спрвері. Спробуйте будь-ласка пізніше');
+          return False;
+        }
+        var modal = $('#myModal'), html = $(data),
+            form = html.find('#content-column form');
+        modal.find('.modal-title').html(html.find('#content-column h2'));
+        modal.find('.modal-body').html(form);
+
+        // init our edit form
+        initEditStudentForm(form, modal, link);
+        
+        modal.modal({
+          'keyboard': false,
+          'backdrop': false,
+          'show': true
+        });
+      },
+      'error': function() {
+        alert('Помилка на сервері. Спробуйте будь-ласка пізніше');
+        return false;
+      }
+    });
+
+    return false;
+  });
+}
+
+function initSubHeaderNav() {
+  $('.nav-tabs a').click(function() {
+    var link = $(this);
+    $.ajax({
+      'url': link.attr('href'),
+      'dataType': 'html',
+      'type': 'get',
+      'success': function(data, status, xhr) {
+        var html = $(data), newpage = html.find('#content-column');
+        $('.nav-tabs li.active').removeClass('active');
+        link.parent('li').addClass('active');
+        link.blur();
+        $('#content-column').html(newpage);
+      }
+    });
+    return false;
   });
 }
 
@@ -61,4 +163,6 @@ $(document).ready(function(){
   initJournal();
   initGroupSelector();
   initDateFields();
+  initEditStudentPage();
+  initSubHeaderNav();
 })
