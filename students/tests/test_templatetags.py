0fa83bfa885c4b813f497377ad30c8c60aeb0c6e
Volodymyr Vlasiuk <trivvet@gmail.com>
2017-08-12 14:51:45 +0300

    Chapter 14 Homework3

--- students/tests/test_templatetags.py
+++ students/tests/test_templatetags.py
@@ -1,7 +1,13 @@
 from django.test import TestCase
+from django.http import HttpRequest
 from django.template import Template, Context
 
+from students.models import Student
+from students.util import paginate
+
 class TemplateTagsTest(TestCase):
+
+    fixtures = ['demo_data3.json']
 
     def test_str2int(self):
         out = Template(
@@ -11,3 +17,24 @@ class TemplateTagsTest(TestCase):
         "{% endif %}").render(Context({}))
         
         self.assertIn('done', out)
+
+    def test_pagenav(self):
+        # prepare list of students for function
+        objects = Student.objects.all()
+
+        # prepare our clean request
+        request = HttpRequest()
+
+        # prepare data for templatetag
+        paginate_data = paginate(objects, 3, request, {})
+
+        out = Template(
+            "{% load pagenav %}"
+            "{% pagenav context.page_obj context.is_paginated context.paginator %}"
+            ).render(Context({'context': paginate_data}))
+
+        # check our tag for obvious content
+        self.assertIn('<!-- Pagination -->', out)
+        self.assertIn('>1</a>\n', out)
+        self.assertIn('<nav aria-label="Page navigation">', out)
+        self.assertIn('<span aria-hidden="true">&raquo;</span>', out)
